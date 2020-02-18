39b9b4466841a8564b25a3e37a376c59573320b7
Surafel Mehari <lesurafel@gmail.com>
2020-02-17 16:08:23 -0800

    Add render currency table to home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
+import Home from './Home';
+
 import './App.css';
 const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route path="/" exact component={Home} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
